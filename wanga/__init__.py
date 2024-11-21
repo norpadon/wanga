48353b59e4bf262960101c33c734dd75d0cd35d0
Artur Chakhvadze <norpadon@yandex.ru>
2024-11-21 15:23:21 +0400

    Export main abstractions to the top level.

--- wanga/__init__.py
+++ wanga/__init__.py
@@ -1,5 +1,11 @@
 from importlib.util import find_spec
 
+from .function import ai_function
+from .models.openai import OpenaAIModel
+from .runtime import Runtime
+
+__all__ = ["ai_function", "OpenaAIModel", "Runtime"]
+
 _PIL_INSTALLED = find_spec("Pillow") is None
 _OPENAI_INSTALLED = find_spec("openai") is None
 _ANTHROPIC_INSTALLED = find_spec("anthropic") is None
